2f1aa5a49cc3fdce00d3ec5e79e20a10ebbfda77
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-04-08 20:39:01 -0300

    Layout definition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,88 @@
 import './App.css';
+import userPlaceholder from './assets/user_placeholder.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <>
-      <h1>Trabalho Final</h1>
+      <div className="main">
+        <h1>Trabalho Final</h1>
+        <h3>Listagem de usuários</h3>
+
+        <div className="user-list">
+          <div className="user-card">
+            <div className="action-buttons">
+              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+            </div>
+            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+            <label>Luiz Guilherme de Bem</label>
+            <label>luizdebem</label>
+            <label>555.555.555.55</label>
+            <label>14/11/1999</label>
+            <label>Masculino</label>
+            <label>Solteiro</label>
+            <label>Senha</label>
+          </div>
+          <div className="user-card">
+            <div className="action-buttons">
+              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+            </div>
+            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+            <label>Luiz Guilherme de Bem</label>
+            <label>luizdebem</label>
+            <label>555.555.555.55</label>
+            <label>14/11/1999</label>
+            <label>Masculino</label>
+            <label>Solteiro</label>
+            <label>Senha</label>
+          </div>
+          <div className="user-card">
+            <div className="action-buttons">
+              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+            </div>
+            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+            <label>Luiz Guilherme de Bem</label>
+            <label>luizdebem</label>
+            <label>555.555.555.55</label>
+            <label>14/11/1999</label>
+            <label>Masculino</label>
+            <label>Solteiro</label>
+            <label>Senha</label>
+          </div>
+          <div className="user-card">
+            <div className="action-buttons">
+              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+            </div>
+            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+            <label>Luiz Guilherme de Bem</label>
+            <label>luizdebem</label>
+            <label>555.555.555.55</label>
+            <label>14/11/1999</label>
+            <label>Masculino</label>
+            <label>Solteiro</label>
+            <label>Senha</label>
+          </div>
+          <div className="user-card">
+            <div className="action-buttons">
+              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+            </div>
+            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+            <label>Luiz Guilherme de Bem</label>
+            <label>luizdebem</label>
+            <label>555.555.555.55</label>
+            <label>14/11/1999</label>
+            <label>Masculino</label>
+            <label>Solteiro</label>
+            <label>Senha</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/environments.js
@@ -0,0 +1,3 @@
+export const environment = {
+  get URI_API() { return "http://localhost:8080/api/v1" }
+};
