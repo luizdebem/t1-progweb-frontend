fae90e3a14496d598cd76156e4bb0106935520a0
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-04-08 20:50:32 -0300

    retrieving users from backend and rendering

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 import userPlaceholder from './assets/user_placeholder.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // @TODO luizdebem: componente UserCard
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    console.log('BOm dia');
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const data = await fetch('http://localhost:8080/api/v1/usuarios');
+    const users = await data.json();
+    console.log(users);
+    setUsers(users);
+  }
+
   return (
     <>
       <div className="main">
@@ -11,76 +27,22 @@ function App() {
         <h3>Listagem de usuários</h3>
 
         <div className="user-list">
-          <div className="user-card">
-            <div className="action-buttons">
-              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+          {users.map(user => (
+            <div className="user-card" key={user.id}>
+              <div className="action-buttons">
+                <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
+              </div>
+              <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
+              <label>{user.nomeCompleto}</label>
+              <label>{user.login}</label>
+              <label>{user.cpf}</label>
+              <label>{user.dataDeNascimento}</label>
+              <label>{user.sexo}</label>
+              <label>{user.estadoCivil}</label>
+              <label>{user.senha}</label>
             </div>
-            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
-            <label>Luiz Guilherme de Bem</label>
-            <label>luizdebem</label>
-            <label>555.555.555.55</label>
-            <label>14/11/1999</label>
-            <label>Masculino</label>
-            <label>Solteiro</label>
-            <label>Senha</label>
-          </div>
-          <div className="user-card">
-            <div className="action-buttons">
-              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
-            </div>
-            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
-            <label>Luiz Guilherme de Bem</label>
-            <label>luizdebem</label>
-            <label>555.555.555.55</label>
-            <label>14/11/1999</label>
-            <label>Masculino</label>
-            <label>Solteiro</label>
-            <label>Senha</label>
-          </div>
-          <div className="user-card">
-            <div className="action-buttons">
-              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
-            </div>
-            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
-            <label>Luiz Guilherme de Bem</label>
-            <label>luizdebem</label>
-            <label>555.555.555.55</label>
-            <label>14/11/1999</label>
-            <label>Masculino</label>
-            <label>Solteiro</label>
-            <label>Senha</label>
-          </div>
-          <div className="user-card">
-            <div className="action-buttons">
-              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
-            </div>
-            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
-            <label>Luiz Guilherme de Bem</label>
-            <label>luizdebem</label>
-            <label>555.555.555.55</label>
-            <label>14/11/1999</label>
-            <label>Masculino</label>
-            <label>Solteiro</label>
-            <label>Senha</label>
-          </div>
-          <div className="user-card">
-            <div className="action-buttons">
-              <FontAwesomeIcon icon={faTrashAlt} size="lg"></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
-            </div>
-            <img src={userPlaceholder} alt="luizdebem" className="profile-picture"></img>
-            <label>Luiz Guilherme de Bem</label>
-            <label>luizdebem</label>
-            <label>555.555.555.55</label>
-            <label>14/11/1999</label>
-            <label>Masculino</label>
-            <label>Solteiro</label>
-            <label>Senha</label>
-          </div>
+          ))}
         </div>
       </div>
     </>
