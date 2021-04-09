150a41e7297aa2f40506d59e7064bd3646cc3c3d
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-04-08 21:56:15 -0300

    WIP - add user form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 import { ApiService } from './services/ApiService';
+import { Sexo, EstadoCivil } from './helpers/Enums';
 
 function App() {
   // @TODO luizdebem: componente UserCard
@@ -25,21 +26,47 @@ function App() {
   const handleDelete = async (userId) => {
     try {
       await apiService.deleteUserById(userId);
-      const updatedList = users.filter(user => user.id !== userId); 
+      const updatedList = users.filter(user => user.id !== userId);
       setUsers(updatedList);
-    } catch (err) {}
+    } catch (err) { }
   }
 
   return (
     <>
       <div className="main">
         <h1>Trabalho Final</h1>
+
+        <div>
+          <button style={{ padding: 15 }}>Incluir novo usuário</button>
+        </div>
+
+        <h3>Formulário de inclusão</h3>
+
+        <form>
+          <input type="text" placeholder="Login"></input>
+          <input type="text" placeholder="Senha"></input>
+          <input type="text" placeholder="Nome completo"></input>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label style={{ fontSize: 12 }}>Data de nascimento:</label>
+            <input type="date"></input>
+          </div>
+          <select>
+            <option value={null} selected disabled>Gênero</option>
+            {Object.entries(Sexo).map(val => <option value={val[0]}>{val[1]}</option>)}
+          </select>
+          <select>
+            <option value={null} selected disabled>Estado civil</option>
+            {Object.entries(EstadoCivil).map(val => <option value={val[0]}>{val[1]}</option>)}
+          </select>
+          <input type="text" placeholder="CPF"></input>
+        </form>
+
         <h3>Listagem de usuários</h3>
 
         <div className="user-list">
           {users.map(user => (
             <div className="user-card" key={user.id}>
-              <div className="action-buttons">
+              <div className="user-card-action-buttons">
                 <FontAwesomeIcon onClick={() => handleDelete(user.id)} icon={faTrashAlt} size="lg"></FontAwesomeIcon>
                 <FontAwesomeIcon icon={faPencilAlt} size="lg"></FontAwesomeIcon>
               </div>
---
+++ src/helpers/Enums.js
@@ -0,0 +1,13 @@
+export const EstadoCivil = {
+  SOLTEIRO: 'Solteiro',
+  CASADO: 'Casado',
+  DIVORCIADO: 'Divorciado',
+  VIUVO: 'Viúvo'
+}
+
+export const Sexo = {
+  MASCULINO: 'Masculino',
+  FEMININO: 'Feminino',
+  OUTRO: 'Outro',
+  NAO_INFORMADO: 'Não informado'
+}
