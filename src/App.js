ede16ae5fed262768da4f67f9f50a1473e5e3cd6
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-06-28 21:28:03 -0300

    WIP componentização

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 import { ApiService } from './services/ApiService';
 import { Sexo, EstadoCivil } from './helpers/Enums';
+import Form from './components/Form';
 
 function App() {
   // @TODO luizdebem: componente UserCard
@@ -61,60 +62,7 @@ function App() {
     setCPF(user.cpf);
   }
 
-  const validate = (user) => {
-    for (let property in user) {
-      if (user[property] === '') {
-        alert('Todas as entradas são obrigatórias.')
-        return false;
-      }
-    }
-    return true;
-  }
 
-  const cleanForm = () => {
-    setLogin('');
-    setSenha('');
-    setNomeCompleto('');
-    setDataDeNascimento('');
-    setSexo(null);
-    setEstadoCivil(null);
-    setCPF('');
-  }
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-
-    let user = {
-      login,
-      senha,
-      nomeCompleto,
-      dataDeNascimento,
-      estadoCivil,
-      sexo,
-      cpf
-    }
-
-    if (!validate(user)) return;
-
-    if (isEdit) {
-      user = {
-        ...user,
-        id: userID
-      }
-    }
-
-    try {
-      isEdit ? await apiService.updateUser(user) : await apiService.postUser(user);
-      const uniqueUsers = users.filter(u => u.id !== user.id);
-      const updatedUsers = [...uniqueUsers, user];
-      setUsers(updatedUsers);
-      setForm(false);
-
-      cleanForm();
-    } catch (err) {
-      console.log(err);
-    }
-  }
 
   return (
     <>
@@ -126,28 +74,11 @@ function App() {
         </div>
 
         {form &&
+
           <>
             <h3>{isEdit ? 'Formulário de edição' : 'Formulário de inclusão'}</h3>
             <p>* campos obrigatórios</p>
-            <form onSubmit={handleFormSubmit}>
-              <input value={login} type="text" placeholder="Login *" onChange={(e) => { setLogin(e.target.value) }}></input>
-              <input value={senha} type="password" placeholder="Senha *" onChange={(e) => { setSenha(e.target.value) }}></input>
-              <input value={nomeCompleto} type="text" placeholder="Nome completo *" onChange={(e) => { setNomeCompleto(e.target.value) }}></input>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <label style={{ fontSize: 12 }}>Data de nascimento *</label>
-                <input value={dataDeNascimento} type="date" onChange={(e) => { setDataDeNascimento(e.target.value) }}></input>
-              </div>
-              <select value={sexo} onChange={(e) => { setSexo(e.target.value) }}>
-                <option defaultValue={null} disabled>Gênero *</option>
-                {Object.entries(Sexo).map(val => <option key={val[0]} value={val[0]}>{val[1]}</option>)}
-              </select>
-              <select value={estadoCivil} onChange={(e) => { setEstadoCivil(e.target.value) }}>
-                <option defaultValue={null} disabled>Estado civil *</option>
-                {Object.entries(EstadoCivil).map(val => <option key={val[0]} value={val[0]}>{val[1]}</option>)}
-              </select>
-              <input value={cpf} type="text" placeholder="CPF *" onChange={(e) => { setCPF(e.target.value) }}></input>
-              <button type="submit">Enviar</button>
-            </form>
+            <Form isEdit={isEdit} user={user} />
           </>
         }
 
